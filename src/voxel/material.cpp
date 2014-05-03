a6f4cd3fe1d7d2886dd8e41cff2ccd963560c78f
Johannes Jendersie <jojendersie@web.de>
2014-05-03 13:23:08 +0200

    # probably fixed a (non-deterministic) bug: material was emissive occasionally    probably caused by a compiler optimization on bitfields # added NDEBUG flag to the release options

--- src/voxel/material.cpp
+++ src/voxel/material.cpp
@@ -11,31 +11,31 @@ namespace Voxel {
 
 	Material::Material( uint8_t _r, uint8_t _g, uint8_t _b, uint8_t _specular, uint8_t _shininess, bool _emissive, uint8_t _variance )
 	{
-		transparent = 0;
-		emissive = _emissive ? 1 : 0;
-		specular = _specular;			assert(_specular < 16);
-		shininess = _shininess;			assert(_shininess < 16);
-		yVar = _variance;				assert(_variance < 16);
+		SetTransparent(false);
+		SetEmissive(_emissive);
+		SetSpecular(_specular);			assert(_specular < 16);
+		SetShininess(_shininess);		assert(_shininess < 16);
+		SetVar(_variance);				assert(_variance < 16);
 
 		// Encode color to YPbPr:855 discretization.
-		y = uint8_t((_r * 299000 + _g * 587000 + _b * 114000) / 1000000);
-		pb = uint8_t((- _r * 20513 - _g * 40271 + _b * 60784 + 15500000) / 1000000);
-		pr = uint8_t((_r * 60784 - _g * 50899 - _b * 9885 + 15500000) / 1000000);
+		SetY( uint8_t((_r * 299000 + _g * 587000 + _b * 114000) / 1000000) );
+		SetPb( uint8_t((- _r * 20513 - _g * 40271 + _b * 60784 + 15500000) / 1000000) );
+		SetPr( uint8_t((_r * 60784 - _g * 50899 - _b * 9885 + 15500000) / 1000000) );
 	}
 
 
 	Material::Material( uint8_t _r, uint8_t _g, uint8_t _b, uint8_t _a, uint8_t _specular, uint8_t _shininess, bool _emissive )
 	{
-		transparent = 1;
-		emissive = _emissive ? 1 : 0;
-		specular = _specular;			assert(_specular < 16);
-		shininess = _shininess;			assert(_shininess < 16);
-		yVar = _a;						assert(_a < 16);
+		SetTransparent(true);
+		SetEmissive(_emissive);
+		SetSpecular(_specular);			assert(_specular < 16);
+		SetShininess(_shininess);		assert(_shininess < 16);
+		SetVar(_a);						assert(_a < 16);
 
 		// Encode color to YPbPr:855 discretization.
-		y = uint8_t((_r * 299000 + _g * 587000 + _b * 114000) / 1000000);
-		pb = uint8_t((- _r * 20513 - _g * 40271 + _b * 60784 + 15500000) / 1000000);
-		pr = uint8_t((_r * 60784 - _g * 50899 - _b * 9885 + 15500000) / 1000000);
+		SetY( uint8_t((_r * 299000 + _g * 587000 + _b * 114000) / 1000000) );
+		SetPb( uint8_t((- _r * 20513 - _g * 40271 + _b * 60784 + 15500000) / 1000000) );
+		SetPr( uint8_t((_r * 60784 - _g * 50899 - _b * 9885 + 15500000) / 1000000) );
 	}
 
 
@@ -48,37 +48,40 @@ namespace Voxel {
 
 		// Emissivity determines how colors are summed
 		unsigned numEmissive = 0;
-		for( int i=0; i<_num; ++i ) { emissive |= _materials[i].emissive; numEmissive += _materials[i].emissive; }
-		//emissive = (tmp) / _num;
+		for( int i=0; i<_num; ++i ) if(_materials[i].IsEmissive()) ++numEmissive;
+		SetEmissive( numEmissive > 0 );
 
 		tmp = 0;
-		for( int i=0; i<_num; ++i ) tmp += _materials[i].y;
-		y = (tmp + nh) / _num;
+		for( int i=0; i<_num; ++i ) tmp += _materials[i].GetY();
+		tmp = (tmp + nh) / _num;
 		// If only a part of the voxels is emissive reduce luminance -> reduce y
-		if( emissive )
-			y = y * (4 + numEmissive) / (4 + _num);
+		if( numEmissive > 0 )
+			SetY( tmp * (4 + numEmissive) / (4 + _num) );
+		else SetY( tmp );
 
 		tmp = 0;
-		for( int i=0; i<_num; ++i ) tmp += _materials[i].specular;
-		specular = (tmp + nh) / _num;
+		for( int i=0; i<_num; ++i ) tmp += _materials[i].GetSpecular();
+		SetSpecular( (tmp + nh) / _num );
 
 		tmp = 0;
-		for( int i=0; i<_num; ++i ) tmp += _materials[i].shininess;
-		shininess = (tmp + nh) / _num;
+		for( int i=0; i<_num; ++i ) tmp += _materials[i].GetShininess();
+		SetShininess( (tmp + nh) / _num );
 
-		transparent = 1;
-		for( int i=0; i<_num; ++i ) transparent &= _materials[i].transparent;
+		// All must be transparent to inherite
+		tmp = 0;
+		for( int i=0; i<_num; ++i ) if(_materials[i].IsTransparent()) ++tmp;
+		SetTransparent(tmp == _num);
 
 		tmp = 0;
-		for( int i=0; i<_num; ++i ) tmp += _materials[i].pb;
-		pb = (tmp + nh) / _num;
+		for( int i=0; i<_num; ++i ) tmp += _materials[i].GetPb();
+		SetPb( ((tmp + nh) / _num) & 0x1f );
 
 		tmp = 0;
-		for( int i=0; i<_num; ++i ) tmp += _materials[i].pr;
-		pr = (tmp + nh) / _num;
+		for( int i=0; i<_num; ++i ) tmp += _materials[i].GetPr();
+		SetPr( (tmp + nh) / _num );
 
 		tmp = 0;
-		for( int i=0; i<_num; ++i ) tmp += _materials[i].yVar;
-		yVar = (tmp + nh) / _num;
+		for( int i=0; i<_num; ++i ) tmp += _materials[i].GetVar();
+		SetVar( (tmp + nh) / _num );
 	}
 } // namespace Voxel
