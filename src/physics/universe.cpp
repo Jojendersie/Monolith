674e8bdc73d32196ae11b9dfb602401bc9a2ae0d
mmmd <michaelmotejat@gmail.com>
2014-09-09 11:07:35 +0200

    commented out a duplicate operation

--- src/physics/universe.cpp
+++ src/physics/universe.cpp
@@ -67,7 +67,7 @@ namespace Physics{
 					acc2 += mFactor*direction;
 				}
 				Vec3 vel = model->GetVelocity();
-				vel += (acc1 + acc2)*fDT / 2;
+				//vel += (acc1 + acc2)*fDT / 2;
 				vel[0] += (acc1[0] + acc2[0])*fDT / 2;
 				vel[1] += (acc1[1] + acc2[1])*fDT / 2;
 				vel[2] += (acc1[2] + acc2[2])*fDT / 2;
@@ -84,50 +84,6 @@ namespace Physics{
 
 
 
-			///* quick and dirty movement	*/
-			//for (std::vector<Voxel::Model *>::iterator cIter = m_celestials.begin(); cIter != m_celestials.end(); ++cIter){
-			//	Voxel::Model *celestial = *cIter;
-			//	float factor = celestial->GetMass()*m_GravConst;
-			//	for (std::vector<Voxel::Model *>::iterator mIter = m_models.begin(); mIter != m_models.end(); ++mIter){
-			//		Voxel::Model *model = *mIter;
-			//		if (celestial == model){
-			//			continue;
-			//		}
-			//		//TODO: Fix bug
-			//		//Vec3 direction = celestial.GetCenter() - model.GetCenter();
-			//		Vec3 direction = Vec3(celestial->GetPosition()[0] - model->GetPosition()[0],
-			//			celestial->GetPosition()[1] - model->GetPosition()[1],
-			//			celestial->GetPosition()[2] - model->GetPosition()[2]);
-
-			//		float mFactor = length(direction);
-			//		mFactor = factor*(model->GetMass()) / mFactor / mFactor / mFactor;
-			//		//std::ostringstream ss;
-			//		//ss << "\n mFactor" << factor << " - " << mFactor << " - " << length(direction);
-			//		//ss << "\n direction:" << direction[0] << "/" << direction[1] << "/" << direction[2];
-			//		direction *= mFactor*_deltaTime/2;
-			//		//TODO: Fix bug
-			//		//model.SetVelocity( model.GetVelocity + direction);
-			//		Vec3 newVel = model->GetVelocity();
-			//		//std::ostringstream ss;
-			//		//ss << "\n speed:" << newVel[0] << "/" << newVel[1] << "/" << newVel[2];
-			//		newVel[0] += direction[0];
-			//		newVel[1] += direction[1];
-			//		newVel[2] += direction[2];
-			//		//LOG_LVL2(ss.str());
-			//		model->SetVelocity(newVel);
-			//	}
-			//}
-			//for (std::vector<Voxel::Model *>::iterator mIter = m_models.begin(); mIter != m_models.end(); ++mIter){
-			//	Voxel::Model *model = *mIter;
-			//	//TODO: Fix bug
-			//	//model.SetPosition (model.GetPosition() + FixVec3(model.GetVelocity()*_deltaTime));
-			//	FixVec3 newPosition = model->GetPosition();
-			//	newPosition[0] += Fix(model->GetVelocity()[0] * _deltaTime);
-			//	newPosition[1] += Fix(model->GetVelocity()[1] * _deltaTime);
-			//	newPosition[2] += Fix(model->GetVelocity()[2] * _deltaTime);
-			//	model->SetPosition(newPosition);
-
-			//}
 			/*quick and dirty collision detection*/
 			//for (std::vector<Voxel::Model *>::iterator mIter = m_models.begin(); mIter != m_models.end();){
 			//	for (std::vector<Voxel::Model *>::iterator mIter2 = ++mIter; mIter2 != m_models.end(); ++mIter2){
